2a88d868b3b2eadb48f505f6d095702497870cdd
Luis <luis.henriquenc10@gmail.com>
2025-04-21 16:54:49 -0300

    Sem Api por enquanto

--- Api/api.js
+++
@@ -1,6 +0,0 @@
-import express from "express"; //Busca o express para dentro do arquivo
-
-const app = express() //Coloca em uma constante
-const PORT = 3000;
-
-app.listen(PORT, () -> console.log(`Servidor está rodando na porta ${PORT}`));
